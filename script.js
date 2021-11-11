2aa5e7e917bb286e2109bc0e927ceba0e597ce97
Jarif Ahmad <jarifahmad@yahoo.com>
2021-11-11 18:36:37 +0000

    pretty much done

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const selectionButtons = document.querySelectorAll("[data-selection")
+const finalColumn = document.querySelector("[data-final-column]")
+const computerScoreSpan = document.querySelector("[data-computer-score]")
+const yourScoreSpan = document.querySelector("[data-your-score]")
 const SELECTIONS = [ //by having the const name in all caps, it makes it a global variable meaning that it wont change (plus its a const = value always stays the same)
     {
         name: "rock",   
@@ -29,7 +32,25 @@ function makeSelection(selection){
     const computerSelection = randomSelection()
     const yourWinner = isWinner(selection, computerSelection)
     const computerWinner = isWinner(computerSelection, selection)
-     console.log(computerSelection)
+    console.log(computerSelection)
+
+    addSelectionResult(computerSelection, computerWinner)
+    addSelectionResult(selection, yourWinner)
+
+    if (yourWinner) incrementScore(yourScoreSpan)
+    if (computerWinner) incrementScore(computerScoreSpan)
+}
+
+function incrementScore(scoreSpan){
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1 
+}
+
+function addSelectionResult(selection, winner){
+    const div = document.createElement("div")
+    div.innerText = (selection.emoji)
+    div.classList.add("result-selection")
+    if (winner) div.classList.add("winner")
+    finalColumn.after(div)
 }
 
 function isWinner(selection, opponentSelection){
